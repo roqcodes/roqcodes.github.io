31290432f2246074a94d49caf7d941157283fb03
Danishx3 <danishkpmariyad@gmail.com>
2024-10-26 06:30:52 +0530

    12

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const firebaseConfig = {
     apiKey: "AIzaSyBm6bl9gCqzTTo-nrMq9LLba7JetZ5mAwM",
     authDomain: "infinitereality-ikgxcs.firebaseapp.com",
@@ -12,25 +11,37 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
+function redirect(value){
 
-const storyRef = database.ref('story');
+
+
+console.log(value);
+let x = value;
+console.log(x);
+
+console.log(x);}
+
+
+const stories = ["s1","s2","s3","s4","s5"];
+let z = stories[Math.floor(Math.random() * stories.length)];
+const storyRef = database.ref(z);
 
 
 storyRef.on('value', (snapshot) => {
-    const story = snapshot.val();
+const story = snapshot.val();
 
-    document.getElementById('story').innerText = story || "";
+document.getElementById('story').innerText = story || "";
 });
 
 function addToStory() {
-    const userInput = document.getElementById('userInput').value;
-    console.log(userInput);
-    if (userInput) {
-        storyRef.once('value').then((snapshot) => {
-            let currentStory = snapshot.val() || "";
-            currentStory += userInput + " ";
-            storyRef.set(currentStory);
-            document.getElementById('userInput').value = "";
-        });
-    }
+const userInput = document.getElementById('userInput').value;
+console.log(userInput);
+if (userInput) {
+    storyRef.once('value').then((snapshot) => {
+        let currentStory = snapshot.val() || "";
+        currentStory += userInput + " ";
+        storyRef.set(currentStory);
+        document.getElementById('userInput').value = "";
+    });
+}
 }
