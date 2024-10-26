9eee1573980d48455645bbe86fe0318692e4bf97
roqcodes <roq2362005@gmail.com>
2024-10-26 06:46:35 +0530

    4

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBm6bl9gCqzTTo-nrMq9LLba7JetZ5mAwM",
-    authDomain: "infinitereality-ikgxcs.firebaseapp.com",
-    databaseURL: "https://infinitereality-ikgxcs.firebaseio.com",
-    projectId: "infinitereality-ikgxcs",
-    storageBucket: "infinitereality-ikgxcs.appspot.com",
-    messagingSenderId: "678050031744",
-    appId: "1:678050031744:web:1d1f9593faed22b773de65",
-    measurementId: "G-W4W212H36X"
-};
+        apiKey: "AIzaSyBm6bl9gCqzTTo-nrMq9LLba7JetZ5mAwM",
+        authDomain: "infinitereality-ikgxcs.firebaseapp.com",
+        databaseURL: "https://infinitereality-ikgxcs.firebaseio.com",
+        projectId: "infinitereality-ikgxcs",
+        storageBucket: "infinitereality-ikgxcs.appspot.com",
+        messagingSenderId: "678050031744",
+        appId: "1:678050031744:web:1d1f9593faed22b773de65",
+        measurementId: "G-W4W212H36X"
+    };
 
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+    const app = firebase.initializeApp(firebaseConfig);
+    const database = firebase.database();
+function redirect(value){
 
-const storyRef = database.ref('story');
+    
+
+    console.log(value);
+    let x = value;
+    console.log(x);
+
+    console.log(x);}
+
+
+    const stories = ["s1","s2","s3","s4","s5"];
+let z = stories[Math.floor(Math.random() * stories.length)];
+const storyRef = database.ref(z);
 
 
 storyRef.on('value', (snapshot) => {
@@ -34,3 +46,6 @@ function addToStory() {
         });
     }
 }
+
+
+
